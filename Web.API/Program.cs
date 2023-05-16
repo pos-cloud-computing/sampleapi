c0dd54a784222a73cceeaa843399995485a64015
viniciusmaltaaraujo <viniciusmaltaaraujo@gmail.com>
2023-05-16 09:42:40 -0300

    mudando a versao do chassi da api

--- Web.API/Program.cs
+++ Web.API/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-	c.SwaggerDoc("v1", new OpenApiInfo { Title = applicationName, Version = "v1" });
+	c.SwaggerDoc("v1", new OpenApiInfo { Title = applicationName, Version = "v2" });
 });
 
 builder.Services.AddApiVersioning();
